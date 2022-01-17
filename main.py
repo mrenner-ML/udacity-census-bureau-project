16b406042dbae0b5ed7b46c872399827d63accd0
Manuel Renner <manuel.renner@sngular.com>
2022-01-17 18:41:48 +0100

    added dvc for heroku

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 '''
 Module for creating API that processes the data and runs inference
 '''
+import os
+
+if "DYNO" in os.environ and os.path.isdir(".dvc"):
+    os.system("dvc config core.no_scm true")
+    if os.system("dvc pull") != 0:
+        exit("dvc pull failed")
+    os.system("rm -r .dvc .apt/usr/lib/dvc")
 
 import pandas as pd
 import pickle
