8eba98ff5e6dd32deed7b8776dae9e30bc754b32
Manuel Renner <manuel.renner@sngular.com>
2022-01-20 10:11:03 +0100

    added screenshots

---
+++ live_post.py
@@ -0,0 +1,26 @@
+import requests
+import json
+
+data = {
+    "age": 39,
+    "workclass": "State-gov",
+    "fnlgt": 77516,
+    "education": "Bachelors",
+    "education-num": 13,
+    "marital-status": "Never-married",
+    "occupation": "Adm-clerical",
+    "relationship": "Not-in-family",
+    "race": "White",
+    "sex": "Male",
+    "capital-gain": 2174,
+    "capital-loss": 0,
+    "hours-per-week": 40,
+    "native-country": "United-States"
+}
+
+r = requests.post(
+    'https://udacity-census-bureau-data.herokuapp.com/predict/', 
+    data=json.dumps(data))
+
+print(r.status_code)
+print(r.json())
