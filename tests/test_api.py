468ca7b760b0500ae4f72a5bd0b2332cb9ec532f
Manuel Renner <manuel.renner@sngular.com>
2022-01-20 09:56:39 +0100

    fixed tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -7,7 +7,7 @@ client = TestClient(app)
 def test_welcome_message():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == 'HELLO!'
+    assert response.json() == {"message": "Hello Udacity"}
 
 def test_correct_input():
     response = client.post(
